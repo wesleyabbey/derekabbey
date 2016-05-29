6a422e88ce02ac0df023907a84cc160d168a3bd2
wesleyabbey <wesleyabbey.io@gmail.com>
2016-05-28 21:07:48 -0400

    added the ability to send emails by opening up the users email client

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,9 +4,15 @@
 	jQuery(document).ready(function(){
 		jQuery('a[data-gal]').each(function() {
 			jQuery(this).attr('rel', jQuery(this).data('gal'));
-		});  	
+		});
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
 
-		
+  });
+
+
 })(jQuery);
+
+function sendEmail() {
+  var emailBody = encodeURI($('.js-email-message').val());
+  window.location.href = 'mailto:derek@dmabbey.com?body='+ emailBody;
+}
